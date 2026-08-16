8cd2aaf27f2d35c5235d4586748f52286d243706
AMAN BALWAN <amanbalwan01@gmail.com>
2026-08-16 15:21:53 -0700

    Mapreduce using multi-processing

--- mapreduce.py
+++ mapreduce.py
@@ -1,13 +1,21 @@
 from collections import defaultdict
+from multiprocessing import Pool, cpu_count
+
 class Mapreduce:
     def __init__(self):
-        self.friend_list=friend_list
+        self.friend_list = friend_list
+        self.mutual_friends = self.run_map_reduce(self.friend_list)
 
-        self.mutual_friends=self._map_reduce()
+    def chunk_dict(self,data, n_chunks):
+        items = list(data.items())
+        if not items:
+            return []
+        chunk_size = max(1, len(items)//n_chunks + (1 if len(items)%n_chunks else 0) )
+        return [dict(items[i:i+chunk_size]) for i in range(0,len(items),chunk_size)]
         
-    def _map(self):
+    def _map(self,friend_list):
         intermediate = []
-        for person,friends in self.friend_list.items():
+        for person,friends in friend_list.items():
                 friends.sort()
                 person = person.lower()
                 for i in range(len(friends)):
@@ -16,17 +24,51 @@ class Mapreduce:
                         intermediate.append((key,person))
         return intermediate
 
-    def _reduce(self,intermediate):
+    def _shuffle(self,mapped_result):
         grouped = defaultdict(list)
-        for key, person in intermediate:
-            grouped[key].append(person)
+        for chunk_result in mapped_result:
+            for key, person in chunk_result:
+                grouped[key].append(person)
         return grouped
 
+    def _reduce_task(self,items):
+        results = []
+        for key,persons in items.items():
+            results.append((key, sorted(set(persons))))
+        return results
 
-    def _map_reduce(self):
-        intermediate = self._map()
-        grouped = self._reduce(intermediate)
-        return grouped
+    def _flattern(self,results):
+        ans=dict()
+        for chunk in results:
+            for key, value in chunk:
+                ans[key]=value
+
+        return ans
+
+    def run_map_reduce(self, data, n_workers=None):
+        n_workers = n_workers or min(cpu_count(),4)
+
+        input_chunks = self.chunk_dict(data,n_workers)
+
+
+        #map
+        with Pool(n_workers) as pool:
+            mapped_result = pool.map(self._map,input_chunks)
+
+        #shuffle
+        grouped = self._shuffle(mapped_result)
+
+        #reduce
+        input_chunks = self.chunk_dict(grouped,n_workers)
+
+        with Pool(n_workers) as pool:
+            reduced_result = pool.map(self._reduce_task,input_chunks)
+
+        #flattern
+        final = self._flattern(reduced_result)
+
+        return final
+
 
     def get_mutual_friends(self, name1, name2):
         name1 = name1.strip().capitalize()
@@ -229,6 +271,7 @@ if __name__ == "__main__":
         names = person.split()
         if len(names)!=2:
             print("Please enter just two names")
+            continue
 
         result = m.get_mutual_friends(names[0],names[1])
 
